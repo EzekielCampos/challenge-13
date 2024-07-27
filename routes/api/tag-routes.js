9cc8b1d2fc96207d3a131ed7454371a75cb1e67b
ezekiel-campos <ezekielcampos@Ezekiels-MacBook-Pro.local>
2024-07-27 15:05:09 -0700

    all CRUD routes for tag are written

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,21 +16,51 @@ res.json(result);
 
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  const result = await Tag.findByPk(req.params.id,{
+
+    include:[Product]
+    
+  })
+  res.json(result);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new tag
+const result = await Tag.create(req.body);
+
+res.json(result);
+
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a tag's name by its `id` value
+const result = await Tag.update(req.body,{
+
+where:{
+  id:req.params.id
+}
+
+})
+
+res.json(result);
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+
+  const result = await Tag.destroy({
+
+    where:{
+      id:req.params.id
+    }
+  })
+
+  res.json("sucess");
 });
 
 module.exports = router;
